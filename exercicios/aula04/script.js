85c2b419da86a9fb3abc017973474393390187aa
Liam <twich.euedu@gmail.com>
2025-08-28 11:01:26 -0300

    aula04
    asd

---
+++ exercicios/aula04/script.js
@@ -0,0 +1,12 @@
+/* window.onload para ele rodar o codigo quando
+ * ele estiver pronto */
+
+function sum() {
+  let email = document.getElementById("e-mail");
+  let password = document.getElementById("password");
+}
+
+function is_higher() {
+  var valor1 = parseInt(document.getElementById("#"));
+  var valor2 = parseInt(document.getElementById("#"));
+}
